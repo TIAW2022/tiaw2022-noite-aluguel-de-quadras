e5ae8f2e1130e8e06743a38243693ec2298924be
root <root@BETELNOTE658.beteltecnologia.net.br>
2022-11-21 20:03:46 -0300

    coluna acoes js

--- codigo/Paginas/Quadras/js/funcoes.js
+++ codigo/Paginas/Quadras/js/funcoes.js
@@ -1,6 +1,6 @@
 var quadras = [];
 
-function listaQuadras(){
+function listaQuadras() {
     var quadras1 = JSON.parse(localStorage.getItem('quadras') || "[]");
     console.log(quadras1)
     var item = '';
@@ -13,29 +13,46 @@ function listaQuadras(){
             <td>${quadra.tipo}</td>
             <td>${quadra.ativo}</td>
             <td>R$ ${quadra.valor}</td>
+            <td class="text-center">
+                <div style="font-size: 22px;display: flex;justify-content: space-evenly;align-items: center;">
+                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    <i class="fa fa-trash-o" aria-hidden="true"></i>
+                </div>
+            </td>
         </tr>`
     });
-    
+
     $('#corpoLista').html(item)
 
 }
-function adicionaQuadra(){
+function validaAtivo(ativo){
+    if(ativo === ''){
+        ativo = ''
+    }
+}
+function adicionaQuadra() {
     var quadraAdd = JSON.parse(localStorage.getItem('quadras') || "[]");
-    let id = quadraAdd.length+1;
+    let id = quadraAdd.length + 1;
     let nome = $('#nome').val();
     let tipo = $('#tipo').val();
     var ativo = $('#ativo').val();
+    validaAtivo(ativo);
     let valor = $('#valor').val();
-    quadraAdd.push({id, nome, tipo, ativo, valor});
+    quadraAdd.push({
+        id,
+        nome,
+        tipo,
+        ativo,
+        valor
+    });
     localStorage.setItem('quadras', JSON.stringify(quadraAdd));
 }
 
-$(document).ready( function () {
-    
-    $("#salvar").on( "click", function() {
+$(document).ready(function () {
+
+    $("#salvar").on("click", function () {
         adicionaQuadra();
     });
     listaQuadras();
-    
-})
 
+})
