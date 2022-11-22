fe2f118529e3adeac3afc89203d6268f683c8b62
Gabriel Svizzero <svizzerogm@gmail.com>
2022-11-21 23:38:58 -0300

    formulario Clientes

--- codigo/Paginas/Clientes/js/doc.js
+++ codigo/Paginas/Clientes/js/doc.js
@@ -1,24 +1,3 @@
-var inputNome = document.querySelector("#nome");
-var inputsobrenome = document.querySelector("#sobrenome");
-
-sobrenome.addEventListener("keypress", function(e) {
-    var keyCode2 = (e.keyCode2 ? e.keyCode2 : e.which);
-
-    if(keyCode2 > 47 && keyCode2 <58){
-        e.preventDefault();
-    }
-})
-
-
-
-
-nome.addEventListener("keypress", function(e) {
-    var keyCode = (e.keyCode ? e.keyCode : e.which);
-  
-  if (keyCode > 47 && keyCode < 58) {
-    e.preventDefault();
-  }
-});
 
 var clientes = [];
 
