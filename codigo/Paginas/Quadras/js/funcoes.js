eeffcf9e51a0ba0235f2a72bd5855f7b1c9ac4a8
root <root@BETELNOTE658.beteltecnologia.net.br>
2022-11-28 21:57:36 -0300

    ajustes formulario validacoa ok

--- codigo/Paginas/Quadras/js/funcoes.js
+++ codigo/Paginas/Quadras/js/funcoes.js
@@ -14,8 +14,8 @@ function listaQuadras() {
             <td>R$ ${quadra.valor}</td>
             <td class="text-center" style="display: flex; justify-content: center;">
                 <div style="font-size: 19px;display: flex;justify-content: center;align-items: center;">
-                    <a id="editar" data-target="${quadra.id}" type="button" class="btn btn-outline-dark" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                    <a id="deletar" data-target="${quadra.id}" type="button" class="btn btn-outline-danger deletar" style="margin-left: 5%" href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                    <button id="editar" data-target="${quadra.id}" type="button" class="btn btn-outline-dark" href="#" disabled><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                    <button id="deletar" data-target="${quadra.id}" type="button" class="btn btn-outline-danger deletar" style="margin-left: 5%" href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
                 </div>
             </td>
         </tr>`
@@ -39,15 +39,19 @@ function adicionaQuadra() {
     var ativo = $('#ativo').val();
     validaAtivo(ativo)
     var valor = $('#valor').val();
-    var validacaoForm = $(function () {
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "Preenchimento obrigatório!",
+        minlength: jQuery.validator.format("É necessário inserir no mínimo {0} caracteres."),
+    })
+
+    $(function () {
+
         $("#form_quadra").validate();
+        $(".error").addClass('col-12')
     });
     var resultadoValidacao = $("#form_quadra").valid();
-    console.log(resultadoValidacao)
-    if(resultadoValidacao === false){
-        alert('Deu ruim!')
-    }
-    {
+    if (resultadoValidacao == true) {
         quadraAdd.push({
             id,
             nome,
@@ -55,23 +59,22 @@ function adicionaQuadra() {
             ativo,
             valor
         });
-        console.log(resultadoValidacao)
         localStorage.setItem('quadras', JSON.stringify(quadraAdd));
     }
 }
 
-function editarQuadra(){}
+function editarQuadra() {}
 
 $(document).ready(function () {
-    
+
     $("#salvar").on("click", function () {
         adicionaQuadra();
         $(this).attr('href', 'quadras.html');
     });
-    $('body').delegate('.deletar', 'click', function() {
+    $('body').delegate('.deletar', 'click', function () {
         quadras = $.parseJSON(localStorage.getItem('quadras'));
-        let idTarget = $(this).attr('data-target')/quadras.length-1;
-        if (quadras.length == 1){
+        let idTarget = $(this).attr('data-target') / quadras.length - 1;
+        if (quadras.length == 1) {
             idTarget = 0;
         };
         quadras.splice(idTarget, 1);
