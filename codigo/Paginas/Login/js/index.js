db64295c20a89c2dfdf52053164ed5434026a698
root <root@BETELNOTE658.beteltecnologia.net.br>
2022-12-12 17:50:19 -0300

    seleção tipo de login

--- codigo/Paginas/Login/js/index.js
+++ codigo/Paginas/Login/js/index.js
@@ -1,21 +1,48 @@
 function validaUsuario() {
-    var acessoConcedido = false; 
-    var usuarios = JSON.parse(localStorage.getItem('dbPF') || "[]");
-    var login = $('#emailLogin').val();
-    var senha = $('#senhaLogin').val();
-    // var tipo = $("input:radio[name=tipo]:checked").val()    
-    for(let i in usuarios.registro){
+    var acessoConcedido = false;
+    clienteMarcado = $('#cliente').prop('checked');
+    propMarcado = $('#proprietario').prop('checked');
+
+    console.log(propMarcado);
+    console.log(clienteMarcado);
+    
+
+    if(clienteMarcado == true){
+        var usuarios = JSON.parse(localStorage.getItem('dbPF') || "[]");
+        var login = $('#emailLogin').val();
+        var senha = $('#senhaLogin').val();
+        for(let i in usuarios.registro){
+            if(usuarios.registro[i].email == login && usuarios.registro[i].password == senha){
+                acessoConcedido = true;
+            }
+        }
+        return acessoConcedido;
+
+    }else if(propMarcado == true){
+        var usuarios = JSON.parse(localStorage.getItem('dbPJ') || "[]");
+        var login = $('#emailLogin').val();
+        var senha = $('#senhaLogin').val();
+        for(let i in usuarios.registro){
         if(usuarios.registro[i].email == login && usuarios.registro[i].password == senha){
             acessoConcedido = true;
+            }
         }
+        return acessoConcedido;
     }
-    return acessoConcedido;
+    
 }
 
 
 $(document).ready(function () {
-    $('#entrar').on('click', function () {
+    $('#proprietario').on('click', function(){
+        $('#proprietario').prop('checked', true)
+    });
 
+    $('#cliente').on('click', function(){
+        $('#cliente').prop('checked', true)
+    });
+
+    $('#entrar').on('click', function () {
         if(validaUsuario() == true){
             $('#entrar').attr('href', '../../Painel/html/index.html');
         }else{
